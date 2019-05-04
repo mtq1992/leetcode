59e5b5933e6c11976861565e7b741d63ade683c7
mtq1992 <1219746766@qq.com>
2019-05-04 10:30:42 +0800

    28 implement-str-str.py

--- 28.implement-str-str.py
+++ 28.implement-str-str.py
@@ -46,5 +46,16 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
+        if not needle:
+            return 0
+        for i in xrange(len(haystack)-len(needle)+1):
+            if haystack[i] == needle[0]:
+                j = 1
+                while j < len(needle) and haystack[i+j] == needle[j]:
+                    j = j+1
+                if j == len(needle):
+                    return i
+        return -1
+
         
 
